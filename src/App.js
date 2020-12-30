c7eaa6f0949b4e15ee29b07846f58d4d16c7631e
Andrzej Stolarek <stolar.code@gmail.com>
2020-12-31 00:33:30 +0100

    to-do-list jsx added

--- src/App.js
+++ src/App.js
@@ -1,9 +1,30 @@
 
 function App() {
   return (
-    <div className="App">
-      Dzień dobry
-    </div>
+    <main classNameName="container">
+      <header>
+        <h1>To-do List</h1>
+      </header>
+      <section className="section">
+        <h2 className="section__header">Add new task</h2>
+        <div className="section__body">
+          <form className="form">
+            <input className="form__input" placeholder="What should I do today?" />
+              <button className="form__button">Add task</button>
+                </form>
+            </div>
+        </section>
+        <section className="section">
+          <div className="section__container">
+            <h2 className="section__header">Task List</h2>
+            <div className="section__buttonsContainer">
+            </div>
+          </div>
+          <div className="section__body">
+            <ul className="list"></ul>
+          </div>
+        </section>
+    </main>
   );
 }
 
