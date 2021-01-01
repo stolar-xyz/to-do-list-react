6727f5148b58601e62fcb73f037d3bab1ea80b23
Andrzej Stolarek <stolar.code@gmail.com>
2021-01-01 21:49:24 +0100

    icons from react-icons/fa added to list items

--- src/List/index.js
+++ src/List/index.js
@@ -1,16 +1,17 @@
 import React from "react";
 import "./style.css";
+import { FaCheck, FaTimes } from "react-icons/fa";
 
-const List = ({tasks, hideDoneTasks}) => (
+const List = ({ tasks, hideDoneTasks }) => (
     <ul className="list">
         {tasks.map(task => (
             <li
                 className={`list__item ${hideDoneTasks && task.done ? "list__item--hidden" : ""}`}>
-                <button className={`list__button list__button--done ${task.done ? "list__button--done-js" : ""}`}>✔</button>
+                <button className={`list__button list__button--done ${task.done ? "list__button--done-js" : ""}`}><FaCheck /></button>
                 <span className={`list__content ${task.done ? "list__content--special" : ""}`}>
                     {task.content}
                 </span>
-                <button className="list__button list__button--remove">✖</button>
+                <button className="list__button list__button--remove"><FaTimes /></button>
             </li>
         ))}
     </ul>
