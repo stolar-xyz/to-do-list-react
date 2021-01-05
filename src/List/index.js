1c4f828ed38f91c00ea4b3211816dca203523568
Andrzej Stolarek <stolar.code@gmail.com>
2021-01-05 12:57:48 +0100

    added keys to tasks

--- src/List/index.js
+++ src/List/index.js
@@ -6,12 +6,13 @@ const List = ({ tasks, hideDoneTasks }) => (
     <ul className="list">
         {tasks.map(task => (
             <li
+                key={task.id}
                 className={`list__item ${hideDoneTasks && task.done ? "list__item--hidden" : ""}`}>
-                <button className={`list__button list__button--done ${task.done ? "list__button--done-js" : ""}`}><FaCheck title="check mark"/></button>
+                <button className={`list__button list__button--done ${task.done ? "list__button--done-js" : ""}`}><FaCheck title="check mark" /></button>
                 <span className={`list__content ${task.done ? "list__content--special" : ""}`}>
                     {task.content}
                 </span>
-                <button className="list__button list__button--remove"><FaTimes title="cross mark"/></button>
+                <button className="list__button list__button--remove"><FaTimes title="cross mark" /></button>
             </li>
         ))}
     </ul>
