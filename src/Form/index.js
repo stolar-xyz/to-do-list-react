c87c38c34177e3a2558d5c9aedf2618f484880e7
Andrzej Stolarek <stolar.code@gmail.com>
2021-02-01 18:45:23 +0100

    set focus on input on button click added

--- src/Form/index.js
+++ src/Form/index.js
@@ -1,8 +1,9 @@
-import React, { useState } from "react";
+import React, { useState, useRef } from "react";
 import { Button, Input, StyledForm } from "./styled";
 
 const Form = ({ addNewTask }) => {
     const [newTaskContent, setNewTaskContent] = useState("");
+    const inputRef = useRef(null)
 
     const onFormSubmit = (event) => {
         event.preventDefault();
@@ -17,10 +18,15 @@ const Form = ({ addNewTask }) => {
     return (
         <StyledForm onSubmit={onFormSubmit}>
             <Input
+                ref={inputRef}
                 placeholder="What should I do today?"
                 value={newTaskContent}
                 onChange={({ target }) => setNewTaskContent(target.value)} />
-            <Button>Add task</Button>
+            <Button
+                onClick={() => inputRef.current.focus()}
+            >
+                Add task
+            </Button>
         </StyledForm>
     )
 };
