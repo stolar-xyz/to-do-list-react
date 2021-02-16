b0a25160a76a81832050c5979e1b7509e5d67818
Andrzej Stolarek <stolar.code@gmail.com>
2021-02-16 20:01:11 +0100

    deleted unused values form Tasks.js

--- src/App/features/tasks/Tasks.js
+++ src/App/features/tasks/Tasks.js
@@ -1,5 +1,4 @@
 import React from "react";
-import { useTasks } from "../../useTasks";
 import Form from "./Form";
 import TaskList from "./TaskList";
 import Buttons from "./Buttons";
@@ -8,33 +7,18 @@ import Header from "../../common/Header";
 import Container from "../../common/Container";
 
 function Tasks() {
-  const {
-    // tasks,
-    removeTask,
-    toggleTaskDone,
-    markAllAsDone,
-    addNewTask,
-    deleteAll,
-  } = useTasks();
 
   return (
     <Container>
       <Header title="To-do List" />
       <Section
         title="Add new task"
-        body={<Form addNewTask={addNewTask} />}
+        body={<Form />}
       />
       <Section
         title="Task List"
-        body={<TaskList
-          removeTask={removeTask}
-          toggleTaskDone={toggleTaskDone}
-        />}
-
-        extraHeaderContent={<Buttons
-          markAllAsDone={markAllAsDone}
-          deleteAll={deleteAll}
-        />}
+        body={<TaskList />}
+        extraHeaderContent={<Buttons />}
       />
     </Container>
   );
