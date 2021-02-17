ea097455634bce327ad02cca2713aa123ab01357
Andrzej Stolarek <stolar.code@gmail.com>
2021-02-17 22:57:38 +0100

    spacing deleted

--- src/App/features/tasks/tasksSlice.js
+++ src/App/features/tasks/tasksSlice.js
@@ -46,6 +46,4 @@ export const selectHideDone = state => selectTasksState(state).hideDone;
 export const selectIsEveryTaskDone = state => selectTasks(state).every(({ done }) => done);
 export const selectAreTasksEmpty = state => selectTasks(state).length === 0;
 
-
-
 export default tasksSlice.reducer;
