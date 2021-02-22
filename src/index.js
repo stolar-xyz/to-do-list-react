796bcf2ded1277188772897c8421a9159774609c
Andrzej Stolarek <stolar.code@gmail.com>
2021-02-22 16:02:36 +0100

    added correct spacing

--- src/index.js
+++ src/index.js
@@ -9,15 +9,15 @@ import { Provider } from "react-redux";
 import store from "./App/store";
 
 ReactDOM.render(
-  <React.StrictMode>
-    <Provider store={store}>
-      <ThemeProvider theme={theme}>
-        <GlobalStyle />
-        <App />
-      </ThemeProvider>
-    </Provider>
-  </React.StrictMode>,
-  document.getElementById('root')
+    <React.StrictMode>
+        <Provider store={store}>
+            <ThemeProvider theme={theme}>
+                <GlobalStyle />
+                <App />
+            </ThemeProvider>
+        </Provider>
+    </React.StrictMode>,
+    document.getElementById('root')
 );
 
 // If you want to start measuring performance in your app, pass a function
