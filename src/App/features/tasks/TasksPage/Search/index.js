9e837fc3f1bc6af18d1f42d3c08c67ddf55260cf
Andrzej Stolarek <stolar.code@gmail.com>
2021-02-24 19:23:12 +0100

    added custom hooks to Search component

--- src/App/features/tasks/TasksPage/Search/index.js
+++ src/App/features/tasks/TasksPage/Search/index.js
@@ -1,24 +1,18 @@
 import React from "react";
-import { useHistory, useLocation } from "react-router-dom";
 import Input from "../Input";
 import searchQueryParamName from "../searchQueryParamName";
+import { useQueryParameters, useReplaceQueryParameters } from "../queryParameters";
 import { Wrapper } from "./styled";
 
 const Search = () => {
-    const location = useLocation();
-    const history = useHistory();
-    const query = (new URLSearchParams(location.search)).get(searchQueryParamName);
+    const query = useQueryParameters(searchQueryParamName);
+    const replaceQueryParameters = useReplaceQueryParameters();
 
     const onInputChange = ({ target }) => {
-        const searchParams = new URLSearchParams(location.search);
-
-        if (target.value.trim() === "") {
-            searchParams.delete(searchQueryParamName);
-        } else {
-            searchParams.set(searchQueryParamName, target.value);
-        };
-
-        history.push(`${location.pathname}?${searchParams.toString()}`);
+        replaceQueryParameters({
+            key: searchQueryParamName,
+            value: target.value.trim() !== "" ? target.value : undefined,
+        });
     };
 
     return (
