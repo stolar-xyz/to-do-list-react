5d286fc73cba65ad54aeeaa84616f01f72d04a57
Andrzej Stolarek <stolar.code@gmail.com>
2021-05-15 17:13:47 +0200

    updated information about author

--- src/App/features/author/AuthorPage.js
+++ src/App/features/author/AuthorPage.js
@@ -6,8 +6,8 @@ const AuthorPage = () => (
     <Container>
         <Header title="About author" />
         <Section
-            title="Hi, I'm Andrzej 👋"
-            body={"I'm a 19yo front-end developer who started coding at the middle of 2019 and fell in love with React. I was motivated to start learning because I wanted to create \"something of my own\". Initially, after the failure of creating music in FL Studio, I decided to bet on programming... I became interested in Python, but after a few weeks of learning, I realized it wasn't for me. Then I found a channel on Youtube that explained the basics of HTML and that's how my passion with web development began."}
+            title="Hi, I'm Andrzej"
+            body={"I'm a 19yo front-end developer who started coding at the middle of 2019 and fell in love with React. I was motivated to start learning because I wanted to create \"something of my own\". Initially, after the failure of creating music in FL Studio, I decided to bet on programming... I became interested in Python, but after a few weeks of learning, I realized it wasn't for me. Then I found a channel on Youtube that explained the basics of HTML and CSS. After that I came across with JavaScript and that's how my passion with web development began."}
         />
     </Container>
 );
